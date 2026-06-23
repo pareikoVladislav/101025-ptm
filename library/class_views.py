806a523eaa27115a00ebd3026c8772262b96c735
kr-master4610 <master4610@gmail.com>
2026-06-23 12:38:17 +0200

    homework-18 try1

--- library/class_views.py
+++ library/class_views.py
@@ -10,7 +10,6 @@ from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.pagination import PageNumberPagination, CursorPagination
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
 
-
 from rest_framework.views import APIView
 from rest_framework.generics import (
     get_object_or_404,
@@ -41,21 +40,24 @@ from library.models import Book, Category, Author, User, Publisher
 from query_debug import QueryDebug
 
 
-
 class CustomPageNumberPaginator(PageNumberPagination):
-    page_size = 15
-    page_size_query_param = 'page-size'
+    page_size = 6
+
+
+class CustomCursorPaginator(CursorPagination):
+    page_size = 3
+    ordering = 'id'
 
 
 class BookListCreateAPIView(APIView):
+    # Задание 2: Мутации данных доступны только авторизованным по JWT пользователям
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def filter_queryset(self):
-        qs = Book.objects.all()
+        qs = Book.objects.all().order_by('id')
 
         query_params = BookQueryParamsSerializer(data=self.request.query_params)
-        print(query_params)
         query_params.is_valid(raise_exception=True)
-        print(query_params.validated_data)
 
         author = query_params.validated_data.get('author')
         sort_by = query_params.validated_data.get('sort_by')
@@ -75,25 +77,22 @@ class BookListCreateAPIView(APIView):
 
         if sort_by:
             ordering = sort_by
-
             if sort_order == "desc":
                 ordering = f"-{sort_by}"
-
             qs = qs.order_by(ordering)
 
         return qs
 
     def get(self, request: Request, *args, **kwargs) -> Response:
-        # books = Book.objects.all()  # -> [Book(1), ..., Book(1000)]
-        books = self.filter_queryset()  # -> [Book(1), ..., Book(1000)]
+        books = self.filter_queryset()
         serializer = BookListSerializer(books, many=True)
         return Response(
-            data=serializer.data,  # -> [{'id', 1}, ..., {'id': 1000}]
+            data=serializer.data,
             status=status.HTTP_200_OK
         )
 
     def post(self, request: Request, *args, **kwargs) -> Response:
-        data = request.data  # {'name': "...", ...}
+        data = request.data
         serializer = BookCreateUpdateSerializer(data=data)
 
         if not serializer.is_valid():
@@ -103,7 +102,6 @@ class BookListCreateAPIView(APIView):
             )
 
         serializer.save()
-
         return Response(
             data=serializer.data,
             status=status.HTTP_201_CREATED
@@ -111,6 +109,8 @@ class BookListCreateAPIView(APIView):
 
 
 class BookRetrieveUpdateDestroyAPIView(APIView):
+    # Задание 2: Изменение конкретной книги разрешено только авторизованным пользователям
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_object(self):
         return get_object_or_404(
@@ -124,10 +124,8 @@ class BookRetrieveUpdateDestroyAPIView(APIView):
             data=data,
             partial=partial
         )
-
         serializer.is_valid(raise_exception=True)
         serializer.save()
-
         return Response(
             data=serializer.data,
             status=status.HTTP_200_OK
@@ -144,91 +142,54 @@ class BookRetrieveUpdateDestroyAPIView(APIView):
     def put(self, request: Request, *args, **kwargs) -> Response:
         book = self.get_object()
         data = request.data
-
-        return self.update(
-            book,
-            data
-        )
+        return self.update(book, data)
 
     def patch(self, request: Request, *args, **kwargs) -> Response:
         book = self.get_object()
         data = request.data
-
-        return self.update(
-            book,
-            data,
-            partial=True
-        )
+        return self.update(book, data, partial=True)
 
     def delete(self, request: Request, *args, **kwargs) -> Response:
         book = self.get_object()
-
         book.delete()
-
         return Response(
             data={},
             status=status.HTTP_204_NO_CONTENT
         )
 
 
-
-
 class CategoryListCreateGenericAPIView(GenericAPIView):
-
-    queryset = Category.objects.all()
+    queryset = Category.objects.all().order_by('id')
     serializer_class = CategorySerializer
-    pagination_class = CustomPageNumberPaginator
+    # Задание 2: Чтение доступно всем, создание категорий — по токену
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get(self, request: Request, *args, **kwargs) -> Response:
         categories = self.get_queryset()
-
         pag = self.paginate_queryset(categories)
-
         serializer = self.get_serializer(pag, many=True)
-
         return self.get_paginated_response(serializer.data)
-
-        # return Response(
-        #     data=serializer.data,
-        #     status=status.HTTP_200_OK
-        # )
 
     def post(self, request: Request, *args, **kwargs) -> Response:
         serializer = self.get_serializer(data=request.data)
-        # if not serializer.is_valid():
-        #     return Response(
-        #         data=serializer.errors,
-        #         status=status.HTTP_400_BAD_REQUEST
-        #     )
-
         serializer.is_valid(raise_exception=True)
         serializer.save()
-
         return Response(
             data=serializer.data,
             status=status.HTTP_201_CREATED
         )
 
 
-
 class CategoryRetrieveUpdateDestroyGenericView(RetrieveUpdateDestroyAPIView):
-
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-
-    # по какой колонке в БД будет идти поиск одного объекта
     lookup_field = 'name'
-
-    # как должна называться динамичная переменная в урликах в запросе
     lookup_url_kwarg = 'name'
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class AuthorListCreateGenericView(ListCreateAPIView):
-
-    # queryset = Author.objects.all()  # какой набор данных возьмётся на ВСЮ вьюшку целиком
-    # serializer_class = AuthorSerializer  # какой сериализатор будет взят на ВСЮ вьюшку целиком
-
-    pagination_class = CustomPageNumberPaginator
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_serializer_class(self):
         if self.request.method == 'GET':
@@ -236,14 +197,7 @@ class AuthorListCreateGenericView(ListCreateAPIView):
         return AuthorCreateSerializer
 
     def get_queryset(self):
-        """
-        Используем, когда нужно, чтобы запрос на взятие данных мог динамически изменяться
-        :return: итоговый набор данных
-        """
-
-        qs = Author.objects.all()
-
-        # http://127.0.0.1:8000/api/v1/authors/?rating_gt=4
+        qs = Author.objects.all().order_by('id')
         rating_gt = self.request.query_params.get('rating_gt')
         rating_lt = self.request.query_params.get('rating_lt')
 
@@ -252,68 +206,54 @@ class AuthorListCreateGenericView(ListCreateAPIView):
                 rating_gt = int(rating_gt)
                 qs = qs.filter(rating__gt=rating_gt)
             except ValueError:
-                # QuerySet([Obj(1), ..., Obj(100)]) -> -> qs.none() -> -> QuerySet([])
-                qs = qs.none()  # если нам передали плохой рейтинг ("hello") -- "наказываем" за оплошность и ОЧИЩШАЕМ ВЕСЬ НАБОР ДАННЫХ
+                qs = qs.none()
 
         if rating_lt:
             try:
                 rating_lt = int(rating_lt)
                 qs = qs.filter(rating__lt=rating_lt)
             except ValueError:
-                # QuerySet([Obj(1), ..., Obj(100)]) -> -> qs.none() -> -> QuerySet([])
-                qs = qs.none()  # если нам передали плохой рейтинг ("hello") -- "наказываем" за оплошность и ОЧИЩШАЕМ ВЕСЬ НАБОР ДАННЫХ
+                qs = qs.none()
 
         return qs
 
     def create(self, request: Request, *args, **kwargs):
-
         if 'date_for_birth' not in request.data or not request.data.get('date_for_birth'):
             request.data['date_for_birth'] = timezone.now()
-
         return super().create(request, *args, **kwargs)
 
 
-
-
-
-
 class UserListGenericView(ListAPIView):
-
-    queryset = User.objects.prefetch_related('reviews')
+    queryset = User.objects.prefetch_related('reviews').order_by('id')
     serializer_class = UserListSerializer
-
+    # Задание 2: Просмотр списка пользователей строго ограничен администраторами
+    permission_classes = [IsAdminUser]
 
     def get_serializer_context(self):
-
         context = super().get_serializer_context()
         include_related = self.request.query_params.get('related', 'false')
         context['include_related'] = include_related.lower() == 'true'
-
         return context
 
-    # этот класс мы можем использовать как декоратор. В этом помогает магический метод __call__
-    # класс мы именно вызываем и можем (не обязательно) передать парметр file_name -- место, куда логи будут записываться
     @QueryDebug(file_name='user-list-query.log')
     def list(self, request, *args, **kwargs):
         return super().list(request, *args, **kwargs)
 
 
-
-class CustomCursorPaginator(CursorPagination):
-    page_size = 10
-    ordering = 'id'
-
-
 class BookListGenericView(ListAPIView):
-
-    queryset = Book.objects.all()
+    queryset = Book.objects.all().order_by('id')
     serializer_class = BookListSerializer
-    pagination_class = CustomCursorPaginator
+    # Задание 2: Просматривать каталог книг могут абсолютно все (включая неавторизованных)
+    permission_classes = [AllowAny]
+
+    # Задание 3: Мы закомментировали локальный пагинатор CustomPageNumberPaginator.
+    # Теперь этот эндпоинт автоматически применит ГЛОБАЛЬНУЮ пагинацию на 5 элементов из settings.py.
+    # pagination_class = CustomPageNumberPaginator
 
     filter_backends = [
-        DjangoFilterBackend, # фильтрация данных
-        SearchFilter, # поиск объектов
-        OrderingFilter # сортировку объектов
+        DjangoFilterBackend,
+        SearchFilter,
+        OrderingFilter
     ]
 
     filterset_fields = [
@@ -334,97 +274,35 @@ class BookListGenericView(ListAPIView):
     ]
 
 
-
-
-# ================================================================================================
-
-# VEW SETS
-
-# ================================================================================================
-
-
-
 class PublisherViewSet(ModelViewSet):
-    queryset = Publisher.objects.all()
-
-    # HTTP методы заменяются на self.actions
-    #
-    # GET -> list | retrieve | get_statistic_by_publisher
-    # PUT -> update
-    # PATCH -> partial_update
-    # POST -> create
-    # DELETE -> destroy
-
-    # проверка на метод заменяется на проверку на action
-    # if request.method  => => if self.action
+    queryset = Publisher.objects.all().order_by('id')
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
     def get_serializer_class(self):
-        # print(self.action)
-
         if self.action == 'list':
             return PublisherListSerializer
         elif self.action == 'create':
             return PublisherCreateSerializer
         elif self.action in {'update', 'partial_update'}:
             return PublisherUpdateSerializer
-
         return PublisherDetailSerializer
 
-
-    # detail:
-    # True -- работаем с одним конкретным объектом
-    # False -- работаем со МНОГИМИ ОБЪЕКТАМИ
-    @action(detail=True, methods=['get',])
+    @action(detail=True, methods=['get'])
     def get_statistic_by_publisher(self, request: Request, *args, **kwargs) -> Response:
         publisher = self.get_object()
         serializer = self.get_serializer(publisher)
         data = serializer.data
         data['count_of_books'] = publisher.books.count()
+        return Response(data=data, status=status.HTTP_200_OK)
 
-        return Response(
-            data=data,
-            status=status.HTTP_200_OK
-        )
-
-    @action(detail=False, methods=['get',])
+    @action(detail=False, methods=['get'])
     def get_statistic_by_publishers(self, request: Request, *args, **kwargs) -> Response:
         publishers = self.get_queryset()
-
         publishers = publishers.values('name').annotate(count_of_books=Count('books'))
+        return Response(data=publishers, status=status.HTTP_200_OK)
 
-        return Response(
-            data=publishers,
-            status=status.HTTP_200_OK
-        )
-
-
-
-
-# работа с транзакциями
 
 from django.db import transaction, IntegrityError, DatabaseError
-
-
-# transaction.atomic() -- основной инструмент. Генирирует
-# атомарный блок. При любой ошибке в э\том блоке все изменения
-# откатываются АВТОМАТИЧЕСКИ. Механизм умный, сам ставить savepoint
-# перед началом транзакции, на каждой успешной частичке транзакции.
-# Если ошибка -- сам прекрасно выполняет rollback. Если успех, сам
-# прекрасно выполняет commit()
-
-
-# transaction.on_commit() -- регистрирует callback (какая-то функция, которая будет вызвана) объект,
-# который будет вызван при успешном коммите
-# transaction.on_commit(callback=lambda: print('УСПЕХ'))
-
-
-# НЕ БЕЗОПАСНЫЙ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-# transaction.set_autocommit(False) -- низкоуровневое управление транзакциями.
-# Мы отключаем автокоммит и сами настраиваем поведение транзакции. ВАЖНО НЕ
-# ЗАБЫТЬ ВКЛЮЧИТЬ ЕГО НАЗАД КАК ТОЛЬКО ТРАНЗАКЦИЯ БЫЛА ВЫПОЛНЕНА
-# transaction.set_autocommit(True)
-
-
 
 
 def notify_me():
@@ -433,50 +311,18 @@ def notify_me():
     print("=" * 100)
 
 
-
 class AuthorViewSet(ModelViewSet):
-    queryset = Author.objects.all()
+    queryset = Author.objects.all().order_by('id')
     serializer_class = AuthorCreateSerializer
+    # Задание 2: Действия с авторами доступны только администраторам системы
     permission_classes = [IsAdminUser]
-    # authentication_classes = [TokenAuthentication]
-
-    # HTTP methods заменяются на self.actions
-
-    # GET    -> list | retrieve
-    # POST   -> create
-    # PUT    -> update
-    # PATCH  -> partial_update
-    # DELETE -> destroy
-    # POST   -> create_author_with_books
-
 
     @action(detail=False, methods=['post'])
     def create_author_with_books(self, request: Request) -> Response:
-        """
-        Example: {
-            "author": {
-                "name": "Test",
-                "surname": "Author"
-            },
-            "books": [
-                {"name": "Book 1", "price": 9.99, "category": 1, "libraries": [1, 2]},
-                {"name": "Book 2", "price": 15.31, "category": 2, "libraries": [2, 3]},
-                {"name": "Book 3", "price": "dvadtsat' pyat'", "category": 3, "libraries": [1, 2, 3]}
-            ]
-        }
-        :param request:
-        :return:
-        """
-
         author_data = request.data.get('author')
         books_data = request.data.get('books')
 
-        # print("=" * 100)
-        # print(author_data)
-        # print("=" * 100)
-
-
-        if not author_data or not (books_data, list):
+        if not author_data or not isinstance(books_data, list):
             return Response(
                 data={'message': 'Запрос должен содержать автора и СПИСОК книг'},
                 status=status.HTTP_400_BAD_REQUEST
@@ -485,21 +331,16 @@ class AuthorViewSet(ModelViewSet):
         try:
             with transaction.atomic():
                 # step 1 создание автора
-
                 author_serializer = self.get_serializer(data=author_data)
-                # author_serializer = AuthorCreateSerializer(data=author_data)
                 author_serializer.is_valid(raise_exception=True)
                 author = author_serializer.save()
 
                 # step 2 создание книжек для этого автора
-
                 for book in books_data:
                     book_serializer = BookCreateUpdateSerializer(data=book)
                     book_serializer.is_valid(raise_exception=True)
                     book_serializer.save(author=author)
 
-
-                # transaction.on_commit(lambda : notify_me())
                 transaction.on_commit(notify_me)
 
         except ValidationError as err:
